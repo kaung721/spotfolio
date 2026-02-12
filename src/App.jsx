8c45e6ce880495bdfefbb7e751cad8ba78dc042c
kaung721 <167502418+kaung721@users.noreply.github.com>
2026-02-12 00:31:19 -0500

    player added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <div className='h-[90%] flex'>
         <Sidebar />
       </div>
+      <Player />
     </div>
   )
 }
