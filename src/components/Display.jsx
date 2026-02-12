f6d03d07e1598975059674f705d71d5931b0fe55
kaung721 <167502418+kaung721@users.noreply.github.com>
2026-02-12 02:39:12 -0500

    Added react-router-dom for routing support and updated Player component with new icons and layout adjustments. Updated package-lock.json and package.json accordingly.

---
+++ src/components/Display.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Display = () => {
+  return (
+    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+        <Routes>
+            <Route path='/' element={<DisplayHome/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Display
---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DisplayHome = () => {
+  return (
+    <>
+        <NavBar />
+    </>
+  )
+}
+
+export default DisplayHome
