ddc44a31a8d56620ac2112b7f3afdaad7cf984ad
kaung721 <167502418+kaung721@users.noreply.github.com>
2026-02-27 21:51:22 -0500

    added images and modified sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,6 @@ function Sidebar() {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
         
-        {/* Top Navigation Block */}
         <div className='bg-[#121212] h-[25%] rounded-lg flex flex-col justify-around'>
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
                 <img className='w-8' src={assets.home_icon} alt="Home"/>
@@ -21,12 +20,12 @@ function Sidebar() {
                 <p className='font-bold'>Search</p>
             </div>
         </div>
-
-        {/* Bottom Library Block */}
-        <div className='bg-[#121212] h-[85%] rounded-lg'>
+        <div className='bg-[#121212] flex-1 rounded-lg flex flex-col'>
+            
+            {/* Header (Your Library) */}
             <div className='p-4 flex items-center justify-between'>
                 <div className='flex items-center gap-3'>
-                    <img className='w-8'src={assets.stack_icon} alt="" />
+                    <img className='w-8' src={assets.stack_icon} alt="" />
                     <p className='font-semibold'>Your Library</p>
                 </div>
                 <div className='flex items-center gap-3'>
@@ -35,20 +34,20 @@ function Sidebar() {
                 </div>
             </div>
 
-            <div className='flex flex-col'> 
+            <div className='flex flex-col flex-1 justify-between px-2 pb-6 pt-2'> 
                 {albumsData.map((item, index) => (
                     <div 
                         key={index} 
-                        className='flex items-center p-3 m-1 gap-3 rounded hover:bg-[#3E3E3E] cursor-pointer transition-colors'
+                        className='flex items-center p-3 gap-4 rounded hover:bg-[#3E3E3E] cursor-pointer transition-colors'
                     >
-                        <img className="w-12 h-12 object-cover rounded" src={item.image} alt={item.name} />
-                        <span className="text-white font-semibold text-xl">{item.name}</span>
+                        <img className="w-12 h-12 xl:w-20 xl:h-20 object-cover rounded" src={item.image} alt="" />
+                        <span className="text-white font-semibold text-lg xl:text-xl">{item.name}</span>
                     </div>
                 ))}
             </div>
-            </div>
-            
         </div>
+    </div>
+        
   )
 }
 
